4f269b36e94d1539a59f02964e8b1e7496ead753
patdaburu <pat@daburu.net>
2017-08-07 16:58:35 -0500

    Switching from distutils to setuptools for packaging.

--- ploogz/__init__.py
+++ ploogz/__init__.py
@@ -8,5 +8,5 @@
 Provide a brief description of the module.
 """
 
-__version__ = '0.0.6'  # The working version.
-__release__ = '0.0.6'  # The release version.
+__version__ = '0.0.7'  # The working version.
+__release__ = '0.0.7'  # The release version.
