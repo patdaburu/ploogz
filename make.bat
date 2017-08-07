1b51e170afa8d86260dc7f3fff6f6d9e21a5a11b
patdaburu <pat@daburu.net>
2017-08-07 16:18:25 -0500

    Updated make.bat script.

--- make.bat
+++ make.bat
@@ -1,5 +1,4 @@
-@echo off
-%~dp0\docs\make html
+REM%~dp0docs\make html
 for /f %%i in ('python -c "import sys, ploogz; sys.stdout.write(ploogz.__version__)"') do set VER=%%i
 git tag %VER% -m "Adding a tag so that we can put this on PyPI."
 git push --tags origin master
