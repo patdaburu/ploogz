ad2333a028084ebc645179dab929e0ab847553dc
patdaburu <pat@daburu.net>
2017-08-08 09:53:52 -0500

    Very minor update to docstring in ploogz.__init__

--- ploogz/__init__.py
+++ ploogz/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-.. currentmodule:: __init__.py
+.. currentmodule:: ploogz.__init__.py
 .. moduleauthor:: Pat Daburu <pat@daburu.net>
 
 Provide a brief description of the module.
