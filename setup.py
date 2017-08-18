474e568d8f01243e59bf3efcaae64e046cb51aa7
patdaburu <pat@daburu.net>
2017-08-18 10:46:23 -0500

    Updated requirements.txt.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
 
     # Indicate who your project is intended for
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Plugins',
+    'Topic :: Software Development :: Libraries',
 
     # Pick your license as you wish (should match "license" above)
     'License :: OSI Approved :: MIT License',
